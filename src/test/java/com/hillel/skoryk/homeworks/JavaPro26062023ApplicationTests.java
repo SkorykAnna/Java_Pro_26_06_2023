48d1e33aeedb3f425051e63e9dfa647d6ef9270d
Anna Skoryk <skoryk.a.y@gmail.com>
2023-09-20 15:33:03 +0200

    1st homework without readme file

---
+++ src/test/java/com/hillel/skoryk/homeworks/JavaPro26062023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hillel.skoryk.homeworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPro26062023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
